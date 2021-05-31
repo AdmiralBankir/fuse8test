29f25c68ec19c9648dd1137c34e59b2c547e6adf
Владислав Бодин <vladislavbodin@MacBook-Pro-Vladislav.local>
2021-05-31 08:22:53 +0300

    Переводит svg в спрайт

--- src/components/MoreBtn/MoreBtn.js
+++ src/components/MoreBtn/MoreBtn.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './MoreBtn.css';
+import chevronIcon from '../../img/chevron.svg'
 
 const MoreBtn = props => {
     return (
         <button className="more-btn" type="button">
             See more
-            <svg width="7" height="17" viewBox="0 0 7 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path id="Chevron" d="M1 1L5.043 8.41667L1 15.8333" stroke="#363636" stroke-width="2" stroke-linecap="round" />
+            <svg width="7" height="17" viewBox={chevronIcon.viewBox}>
+                <use xlinkHref={`#${chevronIcon.id}`} />
             </svg>
-
         </button>
     );
 };
